d3bcbfd5b85cab714b16333609e6b6013d2dcfb4
pcaldi <pcaldi@gmail.com>
2024-07-12 21:03:07 -0300

    Instalando fontes no app

--- src/styles/global.js
+++ src/styles/global.js
@@ -11,7 +11,16 @@ export default createGlobalStyle`
 body {
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
   color: ${({ theme }) => theme.COLORS.WHITE};
+
+  -webkit-font-smoothing: antialiased;
 }
+
+body, input, button, textarea {
+  font-family: 'Roboto Slab', sans-serif;
+  font-size: 16px;
+  outline: none;
+}
+
 
 a {
   text-decoration: none;
