c5f6e3f2146a4e83fa32b00e7bdb69c1545b1090
pcaldi <pcaldi@gmail.com>
2024-07-14 12:20:48 -0300

    corrigindo cores da aplicação

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,9 +1,9 @@
 export default {
   COLORS: {
+    BACKGROUND_600: "rgba(255, 133, 155, 0.05)",
+    BACKGROUND_700: "#262529",
     BACKGROUND_900: "#0D0C0F",
     BACKGROUND_800: "#1C1B1E",
-    BACKGROUND_700: "#262529",
-    BACKGROUND_600: "rgba(255, 133, 155, 0.05)",
 
     WHITE: "#F4EDE8",
     PINK: "#FF859B",
