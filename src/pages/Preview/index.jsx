0d107a3b5b9c9b80e2133d6a2919ac1f4b7cc076
pcaldi <pcaldi@gmail.com>
2024-07-14 13:27:15 -0300

    page Preview

--- src/pages/Preview/index.jsx
+++ src/pages/Preview/index.jsx
@@ -1,12 +1,89 @@
+import { Container, Content, Description, Info, Section } from "./styles"
 
+import { ButtonText } from "../../components/ButtonText"
 import { Header } from "../../components/Header"
-import { Container } from "./styles"
+
+import { LuArrowLeft, LuClock } from "react-icons/lu"
+import { Rating } from "../../components/Rating"
+import { Tag } from "../../components/Tag"
 
 
 export function Preview() {
   return (
     <Container>
       <Header />
+
+      <main>
+        <header>
+          <ButtonText icon={LuArrowLeft} title="Voltar" />
+
+        </header>
+        <Content>
+          <h1>Pulp Fiction</h1>
+          <Rating isBigSize grade={5} />
+        </Content>
+
+        <Info>
+          <p>
+            <img src="https://github.com/pcaldi.png" alt="Foto do usuário" />
+            Por Paulo Ricardo
+          </p>
+          <span>
+            <LuClock />
+            14/07/24 às 13:00
+          </span>
+        </Info>
+
+        <Section>
+          <Tag title="Ação" />
+          <Tag title="Drama" />
+          <Tag title="Suspense" />
+          <Tag title="Ficção" />
+        </Section>
+
+        <Description>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut possimus commodi magnam iure suscipit iste perspiciatis distinctio alias dolorem ipsam excepturi libero, labore earum quia consequuntur modi expedita quibusdam.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic consectetur praesentium, temporibus voluptates autem aperiam deleniti pariatur facere quo sint architecto dolor vero optio dolorem asperiores atque nesciunt cumque totam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptatem harum neque esse, iusto labore cumque. Corrupti maxime doloribus facere, quibusdam natus explicabo recusandae nisi dignissimos, consequuntur aperiam officia dicta?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius laboriosam corrupti nesciunt iure voluptatibus optio voluptates exercitationem asperiores deserunt possimus placeat assumenda, sunt maxime eveniet error necessitatibus aperiam voluptatem minima?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut possimus commodi magnam iure suscipit iste perspiciatis distinctio alias dolorem ipsam excepturi libero, labore earum quia consequuntur modi expedita quibusdam.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic consectetur praesentium, temporibus voluptates autem aperiam deleniti pariatur facere quo sint architecto dolor vero optio dolorem asperiores atque nesciunt cumque totam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptatem harum neque esse, iusto labore cumque. Corrupti maxime doloribus facere, quibusdam natus explicabo recusandae nisi dignissimos, consequuntur aperiam officia dicta?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius laboriosam corrupti nesciunt iure voluptatibus optio voluptates exercitationem asperiores deserunt possimus placeat assumenda, sunt maxime eveniet error necessitatibus aperiam voluptatem minima?
+          </p>   <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut possimus commodi magnam iure suscipit iste perspiciatis distinctio alias dolorem ipsam excepturi libero, labore earum quia consequuntur modi expedita quibusdam.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic consectetur praesentium, temporibus voluptates autem aperiam deleniti pariatur facere quo sint architecto dolor vero optio dolorem asperiores atque nesciunt cumque totam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptatem harum neque esse, iusto labore cumque. Corrupti maxime doloribus facere, quibusdam natus explicabo recusandae nisi dignissimos, consequuntur aperiam officia dicta?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius laboriosam corrupti nesciunt iure voluptatibus optio voluptates exercitationem asperiores deserunt possimus placeat assumenda, sunt maxime eveniet error necessitatibus aperiam voluptatem minima?
+          </p>   <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut possimus commodi magnam iure suscipit iste perspiciatis distinctio alias dolorem ipsam excepturi libero, labore earum quia consequuntur modi expedita quibusdam.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic consectetur praesentium, temporibus voluptates autem aperiam deleniti pariatur facere quo sint architecto dolor vero optio dolorem asperiores atque nesciunt cumque totam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptatem harum neque esse, iusto labore cumque. Corrupti maxime doloribus facere, quibusdam natus explicabo recusandae nisi dignissimos, consequuntur aperiam officia dicta?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius laboriosam corrupti nesciunt iure voluptatibus optio voluptates exercitationem asperiores deserunt possimus placeat assumenda, sunt maxime eveniet error necessitatibus aperiam voluptatem minima?
+          </p>   <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut possimus commodi magnam iure suscipit iste perspiciatis distinctio alias dolorem ipsam excepturi libero, labore earum quia consequuntur modi expedita quibusdam.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic consectetur praesentium, temporibus voluptates autem aperiam deleniti pariatur facere quo sint architecto dolor vero optio dolorem asperiores atque nesciunt cumque totam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptatem harum neque esse, iusto labore cumque. Corrupti maxime doloribus facere, quibusdam natus explicabo recusandae nisi dignissimos, consequuntur aperiam officia dicta?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius laboriosam corrupti nesciunt iure voluptatibus optio voluptates exercitationem asperiores deserunt possimus placeat assumenda, sunt maxime eveniet error necessitatibus aperiam voluptatem minima?
+          </p>   <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut possimus commodi magnam iure suscipit iste perspiciatis distinctio alias dolorem ipsam excepturi libero, labore earum quia consequuntur modi expedita quibusdam.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic consectetur praesentium, temporibus voluptates autem aperiam deleniti pariatur facere quo sint architecto dolor vero optio dolorem asperiores atque nesciunt cumque totam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptatem harum neque esse, iusto labore cumque. Corrupti maxime doloribus facere, quibusdam natus explicabo recusandae nisi dignissimos, consequuntur aperiam officia dicta?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius laboriosam corrupti nesciunt iure voluptatibus optio voluptates exercitationem asperiores deserunt possimus placeat assumenda, sunt maxime eveniet error necessitatibus aperiam voluptatem minima?
+          </p>   <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut possimus commodi magnam iure suscipit iste perspiciatis distinctio alias dolorem ipsam excepturi libero, labore earum quia consequuntur modi expedita quibusdam.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic consectetur praesentium, temporibus voluptates autem aperiam deleniti pariatur facere quo sint architecto dolor vero optio dolorem asperiores atque nesciunt cumque totam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptatem harum neque esse, iusto labore cumque. Corrupti maxime doloribus facere, quibusdam natus explicabo recusandae nisi dignissimos, consequuntur aperiam officia dicta?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius laboriosam corrupti nesciunt iure voluptatibus optio voluptates exercitationem asperiores deserunt possimus placeat assumenda, sunt maxime eveniet error necessitatibus aperiam voluptatem minima?
+          </p>
+        </Description>
+
+
+
+      </main>
     </Container>
   )
 }
--- src/pages/Preview/styles.js
+++ src/pages/Preview/styles.js
@@ -1,5 +1,97 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: grid;
+  grid-template-rows: 1fr auto;
+  grid-template-areas:
+  "header"
+  "content";
+
+
+> main {
+    max-width: 1137px;
+    margin: 0 auto;
+    padding: 40px 106px 156px 123px;
+
+    grid-area: content;
+    overflow-y: auto;
+
+    &::-webkit-scrollbar {
+      width: 8px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      background-color: ${({ theme }) => theme.COLORS.PINK};
+      border-radius: 8px;
+    }
+  }
+
+
+   header {
+    margin-bottom: 24px;
+  }
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  gap: 19px;
+  margin-bottom: 24px;
+
+   > h1 {
+    font-size: 38px;
+   }
+`;
+
+export const Info = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+
+
+  > p {
+    display: flex;
+    align-items: center;
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+
+    img {
+      margin-right: 6px;
+      width: 16px;
+      height: 16px;
+    }
+  }
+
+  > span {
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+
+     svg {
+       margin-right: 6px;
+     }
+  }
+
+`;
+
+export const Section = styled.div`
+  padding: 40px 0;
+
+`;
+
+export const Description = styled.div`
+
+  p{
+    font-size: 14px;
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+    text-align: justify;
+  }
 
 `;
