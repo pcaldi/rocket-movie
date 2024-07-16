48359e97ae9b9ee0a9d646a17afedd7f467f400c
pcaldi <pcaldi@gmail.com>
2024-07-16 16:30:39 -0300

    criando componente Section

---
+++ src/components/Section/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+
+  > h2 {
+    color: ${({ theme }) => theme.COLORS.GRAY_200};
+    font-size: 20px;
+    font-weight: 400;
+
+    padding: 24px 0;
+  }
+`;
