38461bf76179debbde97a0ad40c81713f3926b85
pcaldi <pcaldi@gmail.com>
2024-07-27 23:34:16 -0300

    cadastrando o usuário no banco de dados

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,13 +1,44 @@
+import { useState } from "react";
+
 import { LuMail, LuLock, LuUser, LuArrowLeft } from "react-icons/lu"
 
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
 
+import { api } from "../../services/api"
 
 import { BackgroundImage, Container, Form } from "./styles";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate()
+
+  function handleSignUp() {
+    // Verifico se foi preenchido todos os campos
+    if (!name || !email || !password) {
+      alert("Preencha todos os campos.");
+      return;
+    }
+
+    // Cadastro o usuário na api
+    api.post("/users", { name, email, password })
+      .then(() => {
+        alert("Cadastro realizado com sucesso!");
+        navigate("/");
+      })
+      .catch(error => {
+        if (error.response) {
+          alert(error.response.data.error);
+        } else {
+          alert("Ocorreu um erro ao cadastrar o usuário.");
+        }
+      });
+  }
+
   return (
     <Container>
       <Form>
@@ -20,21 +51,27 @@ export function SignUp() {
           placeholder="Nome"
           type="text"
           icon={LuUser}
+          onChange={(e) => setName(e.target.value)}
         />
 
         <Input
           placeholder="E-mail"
           type="text"
           icon={LuMail}
+          onChange={(e) => setEmail(e.target.value)}
         />
 
         <Input
           placeholder="Senha"
           type="password"
           icon={LuLock}
+          onChange={(e) => setPassword(e.target.value)}
         />
 
-        <Button title="Cadastrar" />
+        <Button
+          title="Cadastrar"
+          onClick={handleSignUp}
+        />
 
         <footer>
           <Link to="/">
