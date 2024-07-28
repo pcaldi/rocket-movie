b9a39c6fc9972a7e802ef6cb49fa8ffb06f9dbba
pcaldi <pcaldi@gmail.com>
2024-07-28 17:56:36 -0300

    fix styles

--- src/components/Section/styles.js
+++ src/components/Section/styles.js
@@ -1,12 +1,11 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-
   > h2 {
     color: ${({ theme }) => theme.COLORS.GRAY_200};
     font-size: 20px;
     font-weight: 400;
 
-    padding: 24px 0;
+    margin: 24px 0;
   }
 `;
