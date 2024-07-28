764c42fb88adf371e7c0d89a9005e0e23f054d76
pcaldi <pcaldi@gmail.com>
2024-07-28 12:56:15 -0300

    adicionando a navigate na função signOut

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,18 +3,23 @@ import { Container, Brand, Profile, Logout } from "./styles";
 import { LuSearch } from "react-icons/lu"
 
 import { Input } from "../Input";
+import { ButtonText } from "../ButtonText";
 
 import { useAuth } from '../../hooks/auth';
-import { ButtonText } from "../ButtonText";
+
+import { useNavigate } from "react-router-dom";
 
 export function Header() {
 
   const { signOut, user } = useAuth();
 
+  const navigate = useNavigate();
+
   function handleSignOut() {
     const confirm = window.confirm("Tem certeza que deseja sair?");
 
     if (confirm) {
+      navigate("/")
       signOut();
     }
   }
