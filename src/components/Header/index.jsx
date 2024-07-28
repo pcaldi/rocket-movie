d856606cedf51e1b7b3e3ab0a6f03c65693cbc9e
pcaldi <pcaldi@gmail.com>
2024-07-28 11:09:10 -0300

    criado a function signOut e aplicada no header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,22 +4,41 @@ import { LuSearch } from "react-icons/lu"
 
 import { Input } from "../Input";
 
+import { useAuth } from '../../hook/auth';
+import { ButtonText } from "../ButtonText";
+
+import { useNavigate } from "react-router-dom";
+
 
 export function Header() {
+
+  const { signOut } = useAuth();
+  const navigate = useNavigate();
+
+  function handleSignOut() {
+    navigate("/");
+    signOut();
+  }
+
   return (
     <Container>
       <Brand>
         <h1>RocketMovies</h1>
       </Brand>
 
-      <Input icon={LuSearch} placeholder="Pesquise pelo título" />
-
+      <Input
+        icon={LuSearch}
+        placeholder="Pesquise pelo título"
+      />
 
       <Profile to="/profile">
         <div className="profile">
 
           <p>Paulo Ricardo</p>
-          <a href="#">Sair</a>
+          <ButtonText
+            title="Sair"
+            onClick={handleSignOut}
+          />
         </div>
         <img
           src="https://github.com/pcaldi.png"
--- src/hook/auth.jsx
+++ src/hook/auth.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 import { api } from '../services/api';
 
@@ -18,6 +18,11 @@ function AuthProvider({ children }) {
 
       const { user, token } = response.data;
 
+      // Salvo o user no localStorage, passando o user de objeto para texto.
+      localStorage.setItem("@rocketmovies:user", JSON.stringify(user));
+      // Salvo o token no localStorage, como token já é uma string não preciso converter.
+      localStorage.setItem("@rocketmovies:token", token);
+
       api.defaults.headers['Authorization'] = `Bearer ${token}`;
 
       setData({ user, token });
@@ -31,11 +36,30 @@ function AuthProvider({ children }) {
     }
   }
 
+  async function signOut() {
+    localStorage.removeItem("@rocketmovies:user");
+    localStorage.removeItem("@rocketmovies:token");
+
+    setData({});
+  }
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("@rocketmovies:user"));
+    const token = localStorage.getItem("@rocketmovies:token");
+
+    if (user && token) {
+      api.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+      setData({ user, token });
+    }
+  }, [])
+
 
 
   return (
     <AuthContext.Provider value={{
       signIn,
+      signOut,
       user: data.user
     }}>
       {children}
